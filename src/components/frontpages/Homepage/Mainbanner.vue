39c2a78f8176d7b679bc9e087e6f583dd74f364e
ticxko <ticxko@gmail.com>
2025-06-01 18:38:57 +0700

    main banner fix

--- src/components/frontpages/Homepage/Mainbanner.vue
+++ src/components/frontpages/Homepage/Mainbanner.vue
@@ -2,42 +2,16 @@
 import { ref } from 'vue';
 import Logo from '@/layouts/full/logo/Logo.vue';
 
-const technology = ref([
-    {
-        img: icon1,
-        name: 'Vue.js'
-    },
-    {
-        img: icon2,
-        name: 'Vuetify'
-    },
-    {
-        img: icon3,
-        name: 'Vite'
-    },
-    {
-        img: icon4,
-        name: 'Typescript'
-    },
-    {
-        img: icon5,
-        name: 'Pinia'
-    },
-    {
-        img: icon6,
-        name: 'ApexChart'
-    }
-]);
 </script>
 <template>
-    <div class="overflow-hidden bg-background">
+    <div class="overflow-hidden bg-background-mainbanner">
         <v-container class="max-width-1218 pt-md-10 pt-6 pb-xl-0 pb-10">
-            <v-row class="d-flex justify-center align-center">
+            <v-row class="d-flex align-center">
                 <v-col cols="12" lg="6" class="text-align-start">
                     <div class="d-flex align-center" style="height: 100%;">
                             <Logo />
                     </div>
-                    <h1 class="text-56 textPrimary font-weight-medium">
+                    <h1 class="text-56 textPrimary font-weight-medium fraunces-h1">
                         <b>Pelayanan Berkualitas untuk Buah Hati Anda</b>
                     </h1>
                     <div class="d-sm-flex text-left justify-md-start justify-center pt-5 gap-3">
@@ -57,12 +31,43 @@ const technology = ref([
                         <v-btn color="primary" rounded="pill" class="px-8 transform-none m-btn-full" size="large"  href="/auth/login" target="_blank">Daftar Baru</v-btn>
                     </div>
                 </v-col>
+                <!-- 
                 <v-col cols="12" lg="6">
                     <div class="main-banner d-lg-block d-none">
-                        <img src="@/assets/images/front-pages/background/hero-x1.png" alt="banner-right-image" class="rtlImg" />
+                        <img src="@/assets/images/front-pages/background/odelia-mainbanner.png" alt="banner-right-image" class="rtlImg" />
                     </div>
                 </v-col>
+                -->
             </v-row>
         </v-container>
     </div>
 </template>
+
+
+<style scoped>
+h1 {
+    font-family: 'Fraunces', serif;
+}
+
+h2 {
+    font-family: 'Outfit', serif;
+    color:slategrey;
+}
+
+@font-face {
+  font-family: 'Fraunces';
+  src: url('@/assets/fonts/Fraunces_72pt_Soft-Regular.ttf') format('truetype');
+  font-weight: normal;
+  font-style: normal;
+}
+
+
+@font-face {
+  font-family: 'Outfit';
+  src: url('@/assets/fonts/Outfit-Regular.ttf') format('truetype');
+  font-weight: normal;
+  font-style: normal;
+}
+
+
+</style>
