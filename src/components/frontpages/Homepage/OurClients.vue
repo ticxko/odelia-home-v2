0c39b9b5a4a452e0dadba492fea30748d7cacf9a
ticxko <ticxko@gmail.com>
2025-06-02 21:16:06 +0700

    justify right

--- src/components/frontpages/Homepage/OurClients.vue
+++ src/components/frontpages/Homepage/OurClients.vue
@@ -25,7 +25,7 @@ import { Icon } from '@iconify/vue';
                             <div class="d-sm-flex text-muted"><img src="@\assets\images\logos\ibi-logo-v4.png" /></div>
                         </v-col>
                         <v-col cols="10" lg="6" class="pt-15">
-                            <div class="d-sm-flex text-muted"><img src="@\assets\images\logos\satusehat-logo-v4.png" /></div>
+                            <div class="d-sm-flex text-muted justify-right"><img src="@\assets\images\logos\satusehat-logo-v4.png" /></div>
                         </v-col>                  
                     </v-row>
                     
